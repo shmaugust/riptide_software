dbe3834f5cd7712427acc793e59ff09f8061febf
shmaugust <mason.768@osu.edu>
2018-07-04 13:01:04 -0400

    adding pnuematics hardware

--- riptide_hardware/scripts/pnuematics.py
+++ riptide_hardware/scripts/pnuematics.py
@@ -2,35 +2,37 @@
 
 import serial
 import rospy
-from riptide_msgs.msg import Pnuematics
-
-COM_PORT = 'dev/pnuematics'
-ser = serial.Serial(COM_PORT, baudrate=9600, timeout=None)
-def pnueCB(pnueMsg):
-    pnueStart = "++++"
-    pnueEnd = "@@@@"
+from riptide_msgs.msg import Pneumatics
+#set up the symlink
+COM_PORT = '/dev/ttyS1'
+ser = serial.Serial(COM_PORT, baudrate=9600, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+def pneuCB(pneuMsg):
+    pneuStart = "++++"
+    pneuEnd = "@@@@\n"
+    #blank nothing messages
     pin1 = "!nnnn"
     pin2 = "!nnnn"
     pin3 = "!nnnn"
     pin4 = "!nnnn"
     #conditionals to send to
-    if pnueMsg.torpedo_stbd:
+    if pneuMsg.torpedo_stbd:
         pin2 = "!1000"
-    if pnueMsg.torpedo_port:
-        pin1 = "!1000"
-    if pnueMsg.markerdropper:
+    if pneuMsg.torpedo_port:
         pin3 = "!1000"
-    if pnueMsg.manipulator:
+    if pneuMsg.markerdropper:
+        pin1 = "!1000"
+    if pneuMsg.manipulator:
         pin4 = "!1000"
-    final_pnue = pnueStart + pin1 + pin2 + pin3 + pin4 + pnueEnd
-    ser.write(final_pnue)
+    final_pneu = pneuStart + pin1 + pin2 + pin3 + pin4 + pneuEnd
+    print "sent: ", final_pneu
+    ser.write(final_pneu)
 
 def main():
-    rospy.init_node('pnuematics')
-
-    pnuePub = rospy.Subsciber("/command/pnuematics", Pnuematics, pnueCB, queue_size=1)
+    rospy.init_node('pneumatics')
+    pneuSub = rospy.Subscriber("/command/pneumatics", Pneumatics, pneuCB, queue_size=1)
+    pneuPub = rospy.Publish("/state/pnuematics", PnueStatus, queue_size=1)
     rate = rospy.Rate(100)
-    pnueMsg = Pneumatics()
+    pneuMsg = Pneumatics()
     while not rospy.is_shutdown():
         rate.sleep()
 
